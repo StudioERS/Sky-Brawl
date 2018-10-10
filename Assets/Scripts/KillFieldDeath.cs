0d8226f2982b7c5ccca4207032e76984d9f83aa2
AlexandreCote <acoteomg@gmail.com>
2018-10-10 17:41:30 -0400

    Camera move

---
+++ Assets/Scripts/KillFieldDeath.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+
+public class KillFieldDeath : MonoBehaviour {
+    [SerializeField] Transform spawn;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.gameObject.tag);
+        if (other.gameObject.tag == "player")
+        {
+            Destroy(other.gameObject);
+          //  RpcKillPlayer();
+        }
+    }
+
+  /*  [ClientRpc] void RpcKillPlayer()
+    {
+        if (isLocalPlayer)
+        {
+            transform.position = spawn.position;
+        }
+    } */
+
+}
