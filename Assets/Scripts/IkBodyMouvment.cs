19a410c319b0bd88c8dfe419783e807b69d32641
AWhiteSheep <43411943+AWhiteSheep@users.noreply.github.com>
2018-10-28 12:00:48 -0400

    So new player with bones
    the new player was needed because the last didnt have any bones so we didnt have any control over the player mouvment.

---
+++ Assets/Scripts/IkBodyMouvment.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IkBodyMouvment : MonoBehaviour {
+
+    Animator anim;
+
+    public float ikWeight = 1;
+
+
+    // Spine 
+    public Transform lookTarget;
+    public float lookWeight;
+    public float bodyWeight;
+    public float headWeight;
+    public float eyesWeight;
+    public float clampWeight;
+
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnAnimatorIK()
+    {
+        // Spine handler
+        // Set our Look Weights for this pass
+        anim.SetLookAtWeight(lookWeight, bodyWeight, headWeight, eyesWeight, clampWeight);
+        //Now set our position to look at for this pass
+        anim.SetLookAtPosition(lookTarget.position);
+
+    }
+}
