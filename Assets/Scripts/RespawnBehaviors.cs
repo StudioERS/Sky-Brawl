77cd3264211a50f03cbe54547fb4e6776fc71e84
AWhiteSheep <43411943+AWhiteSheep@users.noreply.github.com>
2018-11-07 13:25:07 -0500

    Setting up for network bullet handling

--- Assets/Scripts/RespawnBehaviors.cs
+++ Assets/Scripts/RespawnBehaviors.cs
@@ -19,7 +19,7 @@ public class RespawnBehaviors : MonoBehaviour {
     {
         if(other.tag == "Player")
             other.transform.position = RespawnPoint[Random.Range(0, RespawnPoint.Count)].position;
-        if (other.tag == "Fun")
+        if (other.name == "FreeCar")
             other.transform.position = car.position;
     }
 }
