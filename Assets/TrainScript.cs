f004ee39e84d0ef527a4f09062bf3f7db2c806d2
AWhiteSheep <43411943+AWhiteSheep@users.noreply.github.com>
2018-11-07 08:06:35 -0500

    some more change

---
+++ Assets/TrainScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainScript : MonoBehaviour {
+
+    public Transform Train;
+    public Collider TrainIndicator;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        //if (TrainIndicator.i)
+    }
+}
